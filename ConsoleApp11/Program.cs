1202c6135dbaa651253ccc0ba53bf937d3a0869e
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 08:22:12 +0200

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[,] 
+        {
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+            { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+            { 1, 2, 3, 4, 5, 5, 6, 7, 7, 7 },
+            { 5, 3, 2, 1, 1, 1, 1, 1, 1, 1 },
+            { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }
+        };
+
+        int count = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            bool isMonotonic = true;
+
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, j] >= matrix[i - 1, j])
+                {
+                    isMonotonic = false;
+                    break;
+                }
+            }
+
+            if (isMonotonic)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество столбцов, элементы которых монотонно убывают: " + count);
+    }
+    }
+    
